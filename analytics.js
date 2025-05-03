51fd7c64f973cd5779af525cd7c9832901e300a3
aryeh1 <aryeh1@users.noreply.github.com>
2025-05-03 15:09:34 +0000

    Add Google Analytics tracking to index.html, about.html, and cat_on_roof.html

---
+++ analytics.js
@@ -0,0 +1,5 @@
+// Google Analytics Tracking Code
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', 'G-35N2C7TDD3'); // Tracking ID
